bfeeef303165313096e8e41968110eb2b01cbc62
Fabio Mazucato <fabio@buildbox.com.br>
2019-10-26 16:39:14 -0300

    Ajuste excercicio 3

--- src/pages/Exercise3/FighterSelector/FighterSelector.jsx
+++ src/pages/Exercise3/FighterSelector/FighterSelector.jsx
@@ -1,46 +1,48 @@
-import React, { useEffect, useState } from "react";
-import Fighter from "../Fighter/Fighter";
-import FighterSlider from "../FighterSlider/FighterSlider";
-import styles from "./FighterSelector.module.scss";
+import React, { useEffect, useState } from 'react'
+import Fighter from '../Fighter/Fighter'
+import FighterSlider from '../FighterSlider/FighterSlider'
+import styles from './FighterSelector.module.scss'
 
 function FighterSelector() {
-    const [fighter, setFighter] = useState("ken");
-    const [fighterSelectionHistory, setFighterSelectionHistory] = useState([]);
+	const [fighter, setFighter] = useState('ken')
+	const [fighterSelectionHistory, setFighterSelectionHistory] = useState([])
 
-    const onFighterChange = fighter => {
-        setFighter(fighter);
-    }
+	const onFighterChange = fighter => {
+		setFighter(fighter)
+		console.log(fighter)
+	}
 
-    useEffect(() => {
-        setTimeout(() => {
-            setFighterSelectionHistory([
-                ...fighterSelectionHistory,
-                {
-                    fighter,
-                    time: (new Date()).toLocaleTimeString()
-                }
-            ]);
-        }, 1000);
-    }, [fighter])
+	useEffect(() => {
+		setTimeout(() => {
+			setFighterSelectionHistory([
+				...fighterSelectionHistory,
+				{
+					fighter,
+					time: new Date().toLocaleTimeString()
+				}
+			])
+		}, 0)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [fighter])
 
-    return (
-        <div className={styles.FighterSelector}>
-            <div className={styles.FighterName}>{fighter}</div>
-            <FighterSlider
-                fighter={fighter}
-                onFighterChange={onFighterChange}>
-                <Fighter fighter={fighter} />
-            </FighterSlider>
-            <div className={styles.HistoryLog}>
-                <span>History:</span>
-                <div>
-                    {fighterSelectionHistory.map(({ fighter, time }) => (
-                        <p key={fighter+time}>{`[${time}] ${fighter} was selected.`}</p>
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className={styles.FighterSelector}>
+			<div className={styles.FighterName}>{fighter}</div>
+			<FighterSlider fighter={fighter} onFighterChange={onFighterChange}>
+				<Fighter fighter={fighter} />
+			</FighterSlider>
+			<div className={styles.HistoryLog}>
+				<span>History:</span>
+				<div>
+					{fighterSelectionHistory.map(({ fighter, time }) => (
+						<p
+							key={fighter + time}
+						>{`[${time}] ${fighter} was selected.`}</p>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default FighterSelector;
+export default FighterSelector
