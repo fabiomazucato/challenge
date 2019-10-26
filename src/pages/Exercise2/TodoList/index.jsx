2252e835926d53f2229da436cf91a883efdc655f
Fabio Mazucato <fabio@buildbox.com.br>
2019-10-26 15:20:18 -0300

    Ajuste exercício 2

--- src/pages/Exercise2/TodoList/index.jsx
+++ src/pages/Exercise2/TodoList/index.jsx
@@ -1,116 +1,121 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import Tag from "../Tag";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import Tag from '../Tag'
 
 const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
+	const result = Array.from(list)
+	const [removed] = result.splice(startIndex, 1)
+	result.splice(endIndex, 0, removed)
 
-    return result;
-};
+	return result
+}
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
-    userSelect: "none",
-    padding: "1.5rem",
-    margin: `0 0 .1rem 0`,
+	// some basic styles to make the items look a bit nicer
+	userSelect: 'none',
+	padding: '1.5rem',
+	margin: `0 0 .1rem 0`,
 
-    // change background colour if dragging
-    background: isDragging ? "lightgreen" : "white",
+	// change background colour if dragging
+	background: isDragging ? 'lightgreen' : 'white',
 
-    display: `flex`,
-    flexDirection: `column`,
+	display: `flex`,
+	flexDirection: `column`,
 
-    // styles we need to apply on draggables
-    ...draggableStyle
-});
+	// styles we need to apply on draggables
+	...draggableStyle
+})
 
 const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? "lightblue" : "lightgrey",
-    padding: `.1rem`,
-    width: `35rem`
-});
+	background: isDraggingOver ? 'lightblue' : 'lightgrey',
+	padding: `.1rem`,
+	width: `35rem`
+})
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [...props.todos]
-        };
-        this.onDragEnd = this.onDragEnd.bind(this);
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			todos: [...props.todos]
+		}
+		this.onDragEnd = this.onDragEnd.bind(this)
+	}
 
-    onDragEnd(result) {
-        // dropped outside the list
-        if (!result.destination) {
-            return;
-        }
+	onDragEnd(result) {
+		// dropped outside the list
+		if (!result.destination) {
+			return
+		}
 
-        const todos = reorder(
-            this.state.todos,
-            result.source.index,
-            result.destination.index
-        );
+		const todos = reorder(
+			this.state.todos,
+			result.source.index,
+			result.destination.index
+		)
 
-        this.state.todos = todos;
+		this.setState({
+			todos
+		})
 
-        this.props.onChangeOrder(this.state.todos);
-    }
+		this.props.onChangeOrder(this.state.todos)
+	}
 
-    // Normally you would want to split things out into separate components.
-    // But in this example everything is just done in one place for simplicity
-    render() {
-        const { todos } = this.state;
-        return (
-            <DragDropContext onDragEnd={this.onDragEnd}>
-                <Droppable droppableId="droppable">
-                    {(provided, snapshot) => (
-                        <div
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
-                            style={getListStyle(snapshot.isDraggingOver)}>
-                            {todos &&
-                                todos.map((item, index) => (
-                                    <Draggable
-                                        key={item.id}
-                                        draggableId={item.id}
-                                        index={index}>
-                                        {(provided, snapshot) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={getItemStyle(
-                                                    snapshot.isDragging,
-                                                    provided.draggableProps
-                                                        .style,
-                                                    item
-                                                )}>
-                                                {item.content}
-                                                <Tag tagName={item.category} />
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
-        );
-    }
+	// Normally you would want to split things out into separate components.
+	// But in this example everything is just done in one place for simplicity
+	render() {
+		const { todos } = this.state
+		return (
+			<DragDropContext onDragEnd={this.onDragEnd}>
+				<Droppable droppableId='droppable'>
+					{(provided, snapshot) => (
+						<div
+							{...provided.droppableProps}
+							ref={provided.innerRef}
+							style={getListStyle(snapshot.isDraggingOver)}
+						>
+							{todos &&
+								todos.map((item, index) => (
+									<Draggable
+										key={item.id}
+										draggableId={item.id}
+										index={index}
+									>
+										{(provided, snapshot) => (
+											<div
+												ref={provided.innerRef}
+												{...provided.draggableProps}
+												{...provided.dragHandleProps}
+												style={getItemStyle(
+													snapshot.isDragging,
+													provided.draggableProps
+														.style,
+													item
+												)}
+											>
+												{item.content}
+												<Tag tagName={item.category} />
+											</div>
+										)}
+									</Draggable>
+								))}
+							{provided.placeholder}
+						</div>
+					)}
+				</Droppable>
+			</DragDropContext>
+		)
+	}
 }
 
 TodoList.propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.object),
-    onChangeOrder: PropTypes.func
-};
+	todos: PropTypes.arrayOf(PropTypes.object),
+	onChangeOrder: PropTypes.func
+}
 
 TodoList.defaultProps = {
-    todos: [],
-    onChangeOrder: () => {}
-};
+	todos: [],
+	onChangeOrder: () => {}
+}
 
-export default TodoList;
+export default TodoList
